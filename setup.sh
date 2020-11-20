a38bd44bd67beb91e6c93630ff375fe42728afb4
aurbuche <buchau23@gmail.com>
2020-11-20 10:45:17 +0100

    20/11 -2

--- setup.sh
+++ setup.sh
@@ -15,13 +15,7 @@ sudo groupadd docker
 sudo usermod -aG docker $USER
 
 # Prerequisite
-sudo apt-get update && \
-	 apt-get install -y apt-transport-https
-curl -s https://packages.cloud.goolge.com/apt/doc/apt-key/gpg
-sudo apt-key add
-echo "deb http://apt.kubernetes.io/kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
-sudo apt-get update
-sudo apt-get install -y kubectl
+curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl
 
 # Install && start minikube
 echo "You're gonna download minikube"
