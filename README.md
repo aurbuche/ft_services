057eb7c6a389653a01e318e0ba17c5d2a7810a20
Aurelien Bucher <aurbuche@z2r11p6.42lyon.fr>
2021-04-27 15:48:24 +0200

    nginx, metallb work, mysql en cours

---
+++ README.md
@@ -0,0 +1,3 @@
+
+#MetalLB
+https://medium.com/@shoaib_masood/metallb-network-loadbalancer-minikube-335d846dfdbe
--- setup.sh
+++ setup.sh
@@ -12,39 +12,44 @@ White="\033[0;37m"
 
 function CLEAN_FILE {
 	printf "${Blue}Clean files\n${White}"
+	kubectl delete service nginx-service && kubectl delete deployment nginx-deployment
 	docker system prune -a
-	pkill nginx
-	pkill mysql
+	pkill -9 nginx
+#	pkill mysql
 	minikube stop
 	minikube delete
 	printf "${Green}File clean!\n"
 }
 
 function MINIKUBE_SETUP {
-	printf "${Blue}Download kubectl & minikube.\n${White}"
-	# kubectl
-	curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl
-	chmod +x ./kubectl
-	# minikube
-	curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64 \
-		&& chmod +x minikube
-	printf "${Green}Minikube setup!\n${White}"
-	printf "${Blue}Let's start minikube & kubectl.\n${White}"
-	minikube start --driver=virtualbox
-	minikube status
+  printf "${Blue}Download kubectl & minikube.\n${White}"
+  curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/darwin/amd64/kubectl \
+    && chmod +x ./kubectl
+  curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-darwin-amd64 \
+    && chmod +x minikube
+  printf "${Green}Minikube setup!\n${White}"
+  printf "${Blue}Let's start minikube & kubectl.\n${White}"
+  minikube start --vm-driver=virtualbox
+  minikube status
 }
 
-# function METALLB_SETUP {
-# 	printf "${Blue}Download metallb.${NoColor}\n"
-# 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
-# 	kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
-# 	kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
-# }
+function METALLB_SETUP {
+  printf "${Blue}Download metallb.${NoColor}\n"
+  kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/namespace.yaml
+  kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.9.5/manifests/metallb.yaml
+  kubectl create secret generic -n metallb-system memberlist --from-literal=secretkey="$(openssl rand -base64 128)"
+  kubectl create -f srcs/metallb/srcs/metallbConfigMap.yaml
+}
 
 function NGINX_SETUP {
-	docker build -t nginx_server srcs/nginx
-	docker images
-	kubectl apply -f srcs/nginx/srcs/nginx.yaml
+  docker build -t nginx_server srcs/nginx
+  docker images
+  kubectl apply -f srcs/nginx/nginx.yaml
+}
+
+function MYSQL_SETUP {
+  docker build -t mysql_db srcs/mysql
+#  kubectl apply -f srcs/mysql/srcs/mysql_service.yaml
 }
 
 #function PHPMYADMIN_SETUP {
@@ -53,22 +58,25 @@ function NGINX_SETUP {
 #}
 
 function NGINX_RESTART {
-	docker stop nginx_server && docker rm nginx_server
-	docker system prune -a
-	D NGINX_SETUP
+  docker image rm nginx_server
+  docker stop nginx_server && docker rm nginx_server
+  docker system prune -a
+  D NGINX_SETUP
 }
 
 # function METALLB_RESTART {
-	
+
 # }
 
 function STOP {
-	docker stop nginx_server && docker rm nginx_server
+  kubectl delete service nginx-service && kubectl delete deployment nginx-deployment
+  docker stop nginx_server && docker rm nginx_server
 	minikube stop
 	minikube delete
 	rm minikube
 	rm kubectl
 }
+
 function D {
   printf "${Purple}[$I] $1\n${NoColor}" | tr '_' ' '
   ((I++))
@@ -87,27 +95,43 @@ then
 		I=0
 		D STOP
 		;;
+    clean)
+      D CLEAN_FILE
+    ;;
+	  metallb)
+	  D METALLB_SETUP
+	  ;;
+	  nginx)
+	    D NGINX_SETUP
+	  ;;
+	  mysql)
+	    D MYSQL_SETUP
+	  ;;
 		restart)
 		case $2 in
 			nginx)
-			NGINX_RESTART
+			kubectl delete service nginx-service && kubectl delete deployment nginx-deployment
+			D NGINX_RESTART
 			;;
 			metallb)
 			# METALLB_RESTART
 			;;
 			*)
+			kubectl delete service nginx-service && kubectl delete deployment nginx-deployment
 			NGINX_RESTART
 			# METALLB_RESTART
 			;;
 		esac
 	esac
 else
-	minikube docker-env
 	I=0
+  eval $(minikube docker-env)
 	D CLEAN_FILE
 	D MINIKUBE_SETUP
+  eval $(minikube docker-env)
+	D METALLB_SETUP
 	D NGINX_SETUP
-	# D METALLB_SETUP
+	D MYSQL_SETUP
 #	D PHPMYADMIN_SETUP
 fi
 
@@ -115,3 +139,7 @@ fi
 
 #	docker run -d --name="nginx_server" -p 80:80 -p 443:443 nginx_server
 #	eval $(minikube docker-env)
+
+#eval $(minikube docker-env)
+#docker build -t nginx_server srcs/nginx
+#kubectl apply -f srcs/nginx/nginx.yaml
